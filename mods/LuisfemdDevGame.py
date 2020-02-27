c5ffa47683a8ca17bb2c0c86bb56a1da9c26c117
Luis Felipe Montoya <luisfemd@gmail.com>
2020-02-26 22:09:08 -0500

    Resuming the example....

--- mods/LuisfemdDevGame.py
+++ mods/LuisfemdDevGame.py
@@ -7,12 +7,12 @@ def bsGetAPIVersion():
 def bsGetGames():
     return [LuisfemdDevGame]
 
-class PlayerLleva(bs.PlayerSpaz):
-
-    def handleMessage(self, message):
-        if isinstance(message,bs.HitMessage):
-            name = self.getName()
-            bs.sreenMessage(name + " la lleva")
+#class PlayerLleva(bs.PlayerSpaz):
+#
+#    def handleMessage(self, message):
+#        if isinstance(message,bs.HitMessage):
+#            name = self.getName()
+#            bs.sreenMessage(name + " la lleva")
 
 
 class LuisfemdDevGame(bs.TeamGameActivity):
@@ -30,8 +30,8 @@ class LuisfemdDevGame(bs.TeamGameActivity):
         # game's starting - let's just set a timer to end it in 5 seconds
         bs.screenMessage("La lleva... Ending in 60 seconds...")
         #bs.Powerup(self, position=(10, 21, 30), powerupType='curse', expire=False)
-        bs.Powerup(position=(0,0,0),powerupType='health').autoRetain()
-        self._endGameTimer = bs.Timer(60000, bs.WeakCall(self.endGame))
+#        bs.Powerup(position=(0,0,0),powerupType='health').autoRetain()
+        self._endGameTimer = bs.Timer(5000, bs.WeakCall(self.endGame))
 
     def endGame(self):
         # game's over - set a score for each team and tell our activity to finish
@@ -39,21 +39,21 @@ class LuisfemdDevGame(bs.TeamGameActivity):
         for team in self.teams: ourResults.setTeamScore(team,0)
         self.end(results=ourResults)
 
-    def onPlayerJoin(self, player):
+#    def onPlayerJoin(self, player):
         #bs.TeamGameActivity.onPlayerJoin(self, player)
-        self.spawnPlayer(player)
+#        self.spawnPlayer(player)AA
 
         
-    def spawnPlayer(self, player):
-        #spaz = self.spawnPlayerSpaz(player)
-        spaz = PlayerLleva(color=player.color, highlight=player.highlight, character=player.character, player=player)
-
-
-        # lets reconnect this player's controls to this
-        # spaz but *without* the ability to attack or pick stuff up
-        #spaz.connectControlsToPlayer(enablePunch=True, enableBomb=False, enablePickUp=True)
-        player.setActor(spaz)
-        #spaz.curse()
+#    def spawnPlayer(self, player):
+#        #spaz = self.spawnPlayerSpaz(player)
+#        spaz = PlayerLleva(color=player.color, highlight=player.highlight, character=player.character, player=player)
+#
+#
+#        # lets reconnect this player's controls to this
+#        # spaz but *without* the ability to attack or pick stuff up
+#        #spaz.connectControlsToPlayer(enablePunch=True, enableBomb=False, enablePickUp=True)
+#        player.setActor(spaz)
+#        #spaz.curse()
 
 
         
